4b276a56b255b0c8ba09e13db51713811e920bb8
Randy Apuzzo <apuzzo@gmail.com>
2020-05-08 15:10:31 -0700

    removing not neccesary permissions fixing multiple page bug

--- background.js
+++ background.js
@@ -13,47 +13,53 @@ var views = chrome.extension.getViews({
 // }
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
-  });
-  chrome.storage.sync.set({viewAffectedResources: false});
-  chrome.storage.sync.set({viewBranch: false});
-  chrome.storage.sync.set({viewLanguage: false});
-
-  chrome.storage.sync.set({autoRefresh: false});
   
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  
+  
+});
+
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.storage.sync.set({viewAffectedResources: false});
+    chrome.storage.sync.set({viewBranch: false});
+    chrome.storage.sync.set({viewLanguage: false});
+    chrome.storage.sync.set({autoRefresh: false});
+    chrome.storage.sync.set({previewURL: false});
+    
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
         pageUrl: {hostSuffix: 'preview.zesty.io'},
       })],
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
+    
   });
-});
 
 chrome.webRequest.onHeadersReceived.addListener(function(details){
-  for(let i = 0; details.responseHeaders.length > i; i++ ){
-    let header = details.responseHeaders[i];
-    if(header.name == 'surrogate-key'){
-      
-      chrome.storage.sync.get('viewAffectedResources', function(data) {
-        // continue to appended as this hits the CSS and JS file, and other requests made
-        if(data.viewAffectedResources) {
-          chrome.storage.sync.set({'viewAffectedResources': data.viewAffectedResources +' ' +header.value});
-        } else {
-          chrome.storage.sync.set({'viewAffectedResources': header.value});
-        }
+    
 
-      })
-      
-      //views[0].document.getElementById('affectedZuids').innerHTML = header.value;
-    }
-    if(header.name == 'z-branch'){
-      chrome.storage.sync.set({'viewBranch': header.value});
-    }
-    if(header.name == 'content-language'){
-      chrome.storage.sync.set({'viewLanguage': header.value});
+  if(details.initiator.match(/\.preview\.zesty\.io/) !== null && details.type == "main_frame") {
+ 
+    chrome.storage.sync.set({previewURL: details.initiator});
+    console.log(details.responseHeaders)
+    for(let i = 0; details.responseHeaders.length > i; i++ ){
+      let header = details.responseHeaders[i];
+      if(header.name == 'surrogate-key'){
+        
+        chrome.storage.sync.get('viewAffectedResources', function(data) {
+         
+          chrome.storage.sync.set({'viewAffectedResources': header.value});
+          
+
+        })
+        
+        //views[0].document.getElementById('affectedZuids').innerHTML = header.value;
+      }
+      if(header.name == 'z-branch'){
+        chrome.storage.sync.set({'viewBranch': header.value});
+      }
+      if(header.name == 'content-language'){
+        chrome.storage.sync.set({'viewLanguage': header.value});
+      }
     }
   }
 },
