44bab48e26e6be61181aa96894de8f47b18701ec
darwin808 <darwin.apolinario12@gmail.com>
2022-04-27 02:16:54 +0800

    initialize development branch with updated scrip tag

--- background.js
+++ background.js
@@ -1,60 +1,9 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// this is the background code...
 
-'use strict';
-
-var views = chrome.extension.getViews({
-    type: "popup"
-});
-
-
-chrome.runtime.onInstalled.addListener(function() {
-
-  
-});
-
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.storage.sync.set({viewAffectedResources: false});
-    chrome.storage.sync.set({viewBranch: false});
-    chrome.storage.sync.set({viewLanguage: false});
-    chrome.storage.sync.set({autoRefresh: false});
-    chrome.storage.sync.set({previewURL: false});
-    
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostSuffix: 'preview.zesty.io'},
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-    
+// listen for our browerAction to be clicked
+chrome.browserAction.onClicked.addListener(function (tab) {
+  // for the current tab, inject the "inject.js" file & execute it
+  chrome.tabs.executeScript(tab.ib, {
+    file: "main.js",
   });
-
-chrome.webRequest.onHeadersReceived.addListener(function(details){
-    
-
-  if(details.initiator && details.initiator.match(/\.preview\.zesty\.io/) !== null && details.type == "main_frame") {
- 
-    chrome.storage.sync.set({previewURL: details.initiator});
-    console.log(details.responseHeaders)
-    for(let i = 0; details.responseHeaders.length > i; i++ ){
-      let header = details.responseHeaders[i];
-      if(header.name == 'surrogate-key'){
-        
-        chrome.storage.sync.get('viewAffectedResources', function(data) {
-         
-          chrome.storage.sync.set({'viewAffectedResources': header.value});
-          
-
-        })
-      }
-      if(header.name == 'z-branch'){
-        chrome.storage.sync.set({'viewBranch': header.value});
-      }
-      if(header.name == 'content-language'){
-        chrome.storage.sync.set({'viewLanguage': header.value});
-      }
-    }
-  }
-},
-{urls: ["https://*/*"]},["responseHeaders"]);
+});
