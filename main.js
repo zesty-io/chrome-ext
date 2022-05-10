5f021310e83a667cc4531dc821f4a6664a68857c
darwin808 <darwin.apolinario12@gmail.com>
2022-05-10 21:05:09 +0800

    add condition to run only in zesty domain

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 // this is the code which will be injected into a given page...
 
-(function () {
-  var head = document.getElementsByTagName("head")[0];
-  var script = document.createElement("script");
-  script.type = "text/javascript";
-  script.onload = function () {
-    callFunctionFromScript();
-  };
+;(function () {
+   var checkZestyDomain = window.location.host.includes("zesty")
+   var head = document.getElementsByTagName("head")[0]
+   var script = document.createElement("script")
+   script.type = "text/javascript"
 
-  script.src =
-    "https://cdn.jsdelivr.net/gh/zesty-io/explorer@latest/dist/explorer.production.js";
-  head.appendChild(script);
-})();
+   script.src =
+      "https://cdn.jsdelivr.net/gh/zesty-io/explorer@latest/dist/explorer.production.js"
+
+   checkZestyDomain && head.appendChild(script)
+   !checkZestyDomain && alert("Website Not Supported. Try again.")
+})()
