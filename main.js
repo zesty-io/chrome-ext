b146a2ae19574106c70066759c9f15c3337cd1eb
darwin808 <darwin.apolinario12@gmail.com>
2022-05-16 20:40:35 +0800

    development update

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 // this is the code which will be injected into a given page...
 
 ;(function () {
-   var checkZestyDomain = window.location.host.includes("zesty")
    var head = document.getElementsByTagName("head")[0]
    var script = document.createElement("script")
    script.type = "text/javascript"
 
-   script.src =
-      "https://cdn.jsdelivr.net/gh/zesty-io/explorer@latest/dist/explorer.production.js"
+   // prod
+   // script.src =
+   //    "https://cdn.jsdelivr.net/gh/zesty-io/explorer@latest/dist/explorer.production.js"
 
-   checkZestyDomain && head.appendChild(script)
-   !checkZestyDomain && alert("Website Not Supported. Try again.")
+   //dev
+   script.src = "http://test.zesty.io:5500/dist/explorer.production.js"
+   head.appendChild(script)
 })()
